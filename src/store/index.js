790a6e206e89d7c6aa38604b58ae0d8f9dee2f07
Michael Arad <dentfull@gmail.com>
2021-01-03 10:52:55 +0200

    Add axios, basic fetch stuff

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,42 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import axios from 'axios'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    books: null,
+    prevURL: null,
+    nextURL: 'http://gutendex.com/books/'
   },
   mutations: {
+    updateBooks (state, payload) {
+      state.books = payload.books
+    },
+    updatePaginationLinks (state, payload) {
+      console.log(payload)
+      state.prevURL = payload.prevURL
+      state.nextURL = payload.nextURL
+    }
+
   },
   actions: {
+    async fetchNextPage ({ commit, state }) {
+      const response = await axios.get(state.nextURL)
+      console.log(response.data)
+      commit({
+        type: 'updatePaginationLinks',
+        prevURL: state.nextURL,
+        nextURL: response.data.next
+      })
+      commit({
+        type: 'updateBooks',
+        books: response.data.results
+      })
+    }
   },
-  modules: {
+  getters: {
+
   }
 })
